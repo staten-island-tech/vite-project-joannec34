93a6ec98f3645715f8a3e681b5c0d827d1aabb04
joannec34 <joannechen9058@gmail.com>
2022-12-02 20:05:44 -0500

    ya

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,23 @@
 import "../styles/style.css";
 import { skins } from "./skins";
 
-//create boxes
+//theme change
+document.querySelector("#theme-btn").addEventListener("click", function () {
+  if (document.body.classList.contains("blue-theme")) {
+    document.body.classList.add("default-theme");
+    document.body.classList.remove("blue-theme");
+  } else {
+    document.body.classList.add("blue-theme");
+    document.body.classList.remove("default-theme");
+  }
+});
+
+//coding buttons
+
+const remove = function () {
+  document.querySelectorAll(".baby").forEach((babybox) => babybox.remove());
+};
+
 const createboxes = function () {
   skins.forEach((skin) =>
     document.querySelector(".parent").insertAdjacentHTML(
@@ -18,31 +34,92 @@ const createboxes = function () {
 };
 createboxes();
 
-//theme change
-document.querySelector("#theme-btn").addEventListener("click", function () {
-  if (document.body.classList.contains("blue-theme")) {
-    document.body.classList.add("default-theme");
-    document.body.classList.remove("blue-theme");
-  } else {
-    document.body.classList.add("blue-theme");
-    document.body.classList.remove("default-theme");
-  }
+document.querySelector("#all-btn").addEventListener("click", function () {
+  remove();
+  createboxes();
 });
 
-//coding buttons fix this shit
 document.querySelector("#select-btn").addEventListener("click", function () {
   remove();
+  const select = skins.filter((skin) => skin.edition.includes("Select"));
+  select.forEach((skin) =>
+    document.querySelector(".parent").insertAdjacentHTML(
+      "beforeend",
+      `<div class="baby">
+  <img class="bundleimage" src="${skin.image}">
+  <div class="info">
+  <h4>${skin.name}</h4>
+  <p class="specific-info">${skin.edition} EDITION // ${skin.price} VP</p>
+  </div>
+  </div>`
+    )
+  );
 });
 
-const remove = function () {
-  //console.log(document.querySelectorAll(".baby"));
-  document.querySelectorAll(".baby").forEach((babybox) => babybox.remove());
-};
+document.querySelector("#deluxe-btn").addEventListener("click", function () {
+  remove();
+  const deluxe = skins.filter((skin) => skin.edition.includes("Deluxe"));
+  deluxe.forEach((skin) =>
+    document.querySelector(".parent").insertAdjacentHTML(
+      "beforeend",
+      `<div class="baby">
+  <img class="bundleimage" src="${skin.image}">
+  <div class="info">
+  <h4>${skin.name}</h4>
+  <p class="specific-info">${skin.edition} EDITION // ${skin.price} VP</p>
+  </div>
+  </div>`
+    )
+  );
+});
 
-const selectskins = skins.map(selectfilter);
+document.querySelector("#premium-btn").addEventListener("click", function () {
+  remove();
+  const premium = skins.filter((skin) => skin.edition.includes("Premium"));
+  premium.forEach((skin) =>
+    document.querySelector(".parent").insertAdjacentHTML(
+      "beforeend",
+      `<div class="baby">
+  <img class="bundleimage" src="${skin.image}">
+  <div class="info">
+  <h4>${skin.name}</h4>
+  <p class="specific-info">${skin.edition} EDITION // ${skin.price} VP</p>
+  </div>
+  </div>`
+    )
+  );
+});
 
-function selectfilter(skins) {
-  skins.filter((skin) => skin.edition.includes("Select"));
-}
+document.querySelector("#exclusive-btn").addEventListener("click", function () {
+  remove();
+  const exclusive = skins.filter((skin) => skin.edition.includes("Exclusive"));
+  exclusive.forEach((skin) =>
+    document.querySelector(".parent").insertAdjacentHTML(
+      "beforeend",
+      `<div class="baby">
+  <img class="bundleimage" src="${skin.image}">
+  <div class="info">
+  <h4>${skin.name}</h4>
+  <p class="specific-info">${skin.edition} EDITION // ${skin.price} VP</p>
+  </div>
+  </div>`
+    )
+  );
+});
 
-console.log(selectskins);
+document.querySelector("#ultra-btn").addEventListener("click", function () {
+  remove();
+  const ultra = skins.filter((skin) => skin.edition.includes("Ultra"));
+  ultra.forEach((skin) =>
+    document.querySelector(".parent").insertAdjacentHTML(
+      "beforeend",
+      `<div class="baby">
+  <img class="bundleimage" src="${skin.image}">
+  <div class="info">
+  <h4>${skin.name}</h4>
+  <p class="specific-info">${skin.edition} EDITION // ${skin.price} VP</p>
+  </div>
+  </div>`
+    )
+  );
+});
