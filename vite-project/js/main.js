807dceae913dfeeec0860f8e7f1a142fd289919a
joannec34 <joannechen9058@gmail.com>
2022-12-13 14:01:30 -0500

    sfdf

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -19,6 +19,10 @@ const dom = {
   parent: document.querySelector(".parent"),
   allbtn: document.querySelector("#all-btn"),
   selectbtn: document.querySelector("#select-btn"),
+  deluxebtn: document.querySelector("#deluxe-btn"),
+  premiumbtn: document.querySelector("#premium-btn"),
+  exclusivebtn: document.querySelector("#exclusive-btn"),
+  ultrabtn: document.querySelector("#ultra-btn"),
 };
 
 //coding buttons
@@ -41,36 +45,57 @@ const createboxes = function (card) {
 };
 skins.forEach(createboxes);
 
+const createboxesusd = function (card) {
+  dom.parent.insertAdjacentHTML(
+    "beforeend",
+    `<div class="baby">
+  <img class="bundleimage" src="${card.image}">
+  <div class="info">
+  <h4>${card.name}</h4>
+  <p class="specific-info">${card.edition} EDITION // $${card.price}</p>
+  </div>
+  </div>`
+  );
+};
+
 dom.allbtn.addEventListener("click", function () {
   remove();
-  skins.forEach(createboxes);
+  if (document.body.classList.contains("vp")) {
+    skins.forEach(createboxes);
+  } else {
+    skins.forEach(createboxesusd);
+  }
 });
 
 dom.selectbtn.addEventListener("click", function () {
   remove();
   const select = skins.filter((skin) => skin.edition.includes("Select"));
-  select.forEach(createboxes);
+  if (document.body.classList.contains("vp")) {
+    select.forEach(createboxes);
+  } else {
+    select.forEach(createboxesusd);
+  }
 });
 
-document.querySelector("#deluxe-btn").addEventListener("click", function () {
+dom.deluxebtn.addEventListener("click", function () {
   remove();
   const deluxe = skins.filter((skin) => skin.edition.includes("Deluxe"));
   deluxe.forEach(createboxes);
 });
 
-document.querySelector("#premium-btn").addEventListener("click", function () {
+dom.premiumbtn.addEventListener("click", function () {
   remove();
   const premium = skins.filter((skin) => skin.edition.includes("Premium"));
   premium.forEach(createboxes);
 });
 
-document.querySelector("#exclusive-btn").addEventListener("click", function () {
+dom.exclusivebtn.addEventListener("click", function () {
   remove();
   const exclusive = skins.filter((skin) => skin.edition.includes("Exclusive"));
   exclusive.forEach(createboxes);
 });
 
-document.querySelector("#ultra-btn").addEventListener("click", function () {
+dom.ultrabtn.addEventListener("click", function () {
   remove();
   const ultra = skins.filter((skin) => skin.edition.includes("Ultra"));
   ultra.forEach(createboxes);
@@ -88,7 +113,15 @@ document.querySelector("#money-btn").addEventListener("click", function () {
     return convertedskins;
   });
   remove();
-  skinsusd.forEach(createboxes);
+  skinsusd.forEach(createboxesusd);
+  //make this for vp and usd class thing :D
+  if (document.body.classList.contains("blue-theme")) {
+    document.body.classList.add("default-theme");
+    document.body.classList.remove("blue-theme");
+  } else {
+    document.body.classList.add("blue-theme");
+    document.body.classList.remove("default-theme");
+  }
 });
 
 //get stuff ?
@@ -120,4 +153,4 @@ const editions = {
       .forEach((skin) => console.log(skin.name));
   },
 };
-//editions.getSelect();
+editions.getSelect();
